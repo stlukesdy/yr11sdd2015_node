232bc947a96dfa6b1edce9fc88610427d244d749
DrLudwig <mludwig@stlukes.nsw.edu.au>
2015-11-12 15:26:38 +1100

    .

--- teacher/node_tasks/task18/program.js
+++ teacher/node_tasks/task18/program.js
@@ -6,15 +6,21 @@ var endsWith = function (word, suffix) {
   return word.indexOf(suffix, word.length - suffix.length) !== -1;
 };
 
-console.log('1-', endsWith('hello', ''));
-console.log('2-', endsWith('hello', '0'));
-console.log('3-', endsWith('hello', 'ello'));
+//console.log('1-', endsWith('hello', ''));
+//console.log('2-', endsWith('hello', '0'));
+//console.log('3-', endsWith('hello', 'ello'));
 
 
 while (answer != -1) {
-  console.log('h');
+  var firstPart1 = answer.substring(0, answer.length - 1);
+  var firstPart2 = answer.substring(0, answer.length - 2);
+  console.log('firstPart1= '+ firstPart1);
+  console.log('firstPart2= '+ firstPart2);
 
-  if ()
+
+  if (endsWith(answer, 'km')) {
+
+  }
  
 
   answer = readlineSync.question('Input: ');
